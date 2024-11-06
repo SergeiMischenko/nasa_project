56541a5a3b074a2cd6a6ef9b4c1214b58d9984cf
Sergey <main0serg@gmail.com>
2024-11-06 18:41:32 +0600

    added modal window

--- slider/static/nasa_project/js/scripts.js
+++ slider/static/nasa_project/js/scripts.js
@@ -1,4 +1,5 @@
-$(document).ready(function () {
+$(document).ready(function() {
+    // Инициализация слайдера
     $('.slider-for').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -6,6 +7,7 @@ $(document).ready(function () {
         fade: true,
         asNavFor: '.slider-nav'
     });
+
     $('.slider-nav').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
@@ -13,6 +15,37 @@ $(document).ready(function () {
         dots: false,
         centerMode: true,
         focusOnSelect: true,
-        arrows: true,
+        arrows: true
+    });
+
+    // Переменная для хранения индекса текущего изображения
+    let currentIndex = 0;
+    const $sliderItems = $('.slider-for .slider-item img');
+
+    $sliderItems.on('click', function() {
+        // Получаем URL изображения и индекс
+        currentIndex = $sliderItems.index(this);
+        const imgSrc = $(this).attr('src');
+
+        $('#modalImage').attr('src', imgSrc);
+        $('#imageModal').modal('show');
+    });
+
+    // Функция для обновления изображения в модальном окне
+    function updateModalImage(index) {
+        const imgSrc = $sliderItems.eq(index).attr('src');
+        $('#modalImage').attr('src', imgSrc);
+    }
+
+    // Обработчик для кнопки «влево»
+    $('#prevImage').on('click', function() {
+        currentIndex = (currentIndex > 0) ? currentIndex - 1 : $sliderItems.length - 1;
+        updateModalImage(currentIndex);
+    });
+
+    // Обработчик для кнопки «вправо»
+    $('#nextImage').on('click', function() {
+        currentIndex = (currentIndex < $sliderItems.length - 1) ? currentIndex + 1 : 0;
+        updateModalImage(currentIndex);
     });
 });
