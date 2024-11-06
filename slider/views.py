c42359486ace8e812bcd5dae0c70023d78edb210
Sergey <main0serg@gmail.com>
2024-11-06 13:42:18 +0600

    added template for slider

--- slider/views.py
+++ slider/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import SliderItem
 
-# Create your views here.
+
+def index(request):
+    slides = SliderItem.objects.all()
+    return render(request, 'index.html', {'slides': slides})
