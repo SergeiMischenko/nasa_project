8468cb531bb33a421221806b2979c1729791e0a2
Sergey <main0serg@gmail.com>
2024-11-07 09:44:00 +0600

    added crop preview images for admin panel

--- slider/admin.py
+++ slider/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.utils.safestring import mark_safe
+from django.utils.html import format_html
+from easy_thumbnails.files import get_thumbnailer
 
 from .models import SliderItem
 from adminsortable2.admin import SortableAdminMixin
@@ -10,6 +11,8 @@ class SliderItemAdmin(SortableAdminMixin, admin.ModelAdmin):
     list_display = ('title', 'get_image', 'my_order')
 
     def get_image(self, obj):
-        return mark_safe(f'<img src="{obj.image.url}" width="50" height="50">')
+        options = {'size': (50, 50), 'crop': True,}
+        thumbnail_url = get_thumbnailer(obj.image).get_thumbnail(options).url
+        return format_html(f'<a href="{obj.image.url}" target="_blank"><img src="{thumbnail_url}"/>', )
 
     get_image.short_description = 'Изображение'
